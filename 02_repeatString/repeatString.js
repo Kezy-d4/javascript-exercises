98487eba47ac909b1da5b893e3341ed93816b0fe
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-11-08 20:17:04 -0500

    repeatString: Pass test 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,18 @@
 const repeatString = function(str, num) {
   let outputString = str;
 
-  for (let i = 1; i < num; i++) {
-    outputString += str;
+  if (num < 1) {
+    outputString = '';
+    return outputString;
+
+  } else {
+    for (let i = 1; i < num; i++) {
+      outputString += str;
+    }
   }
 
   return outputString
 };
-
 
 // Do not edit below this line
 module.exports = repeatString;
