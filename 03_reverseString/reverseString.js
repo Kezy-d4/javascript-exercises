085583ef32ef13df196b098f63e1768ca73b3879
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-11-09 22:59:00 -0500

    reverseString: Pass test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let strReversed = '';
+  let strL = str.length;
 
+  let i = 0;
+
+  while (i < strL) {
+    strReversed += str.at(-1);
+    str = str.slice(0, -1);
+
+    i++;
+  }
+
+  return strReversed;
 };
 
-// Do not edit below this line
+// Do not edit below this line`
 module.exports = reverseString;
