631b9c3cacda0dcc690a8539a933a781344c33d0
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-11-11 13:38:01 -0500

    sumAll: Pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,48 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) { 
 
+  let greaterNumber;
+  let lesserNumber;
+
+  function compareNumbers() {
+    if (num1 > num2) {
+      greaterNumber = num1;
+      lesserNumber = num2;
+    } else {
+      greaterNumber = num2;
+      lesserNumber = num1;
+    }
+  }
+
+  compareNumbers();
+
+  let finalSum = 0;
+  let i = lesserNumber;
+
+  while (i <= greaterNumber) {
+    finalSum += i;
+
+    i++;
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+/*
+PLANNING...
+
+THE TASK:
+Given two positive integer arguments, return the sum of each integer between them (inclusively). E.g. sumAll(1, 4) should return the sum of 1 + 2 + 3 + 4 which is 10. 
+
+THE PROCESS:
+
+
+
+
+
+
+
+
+
+*/
