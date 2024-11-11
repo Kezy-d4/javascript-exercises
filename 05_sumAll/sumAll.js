932412164fbe7b105f1bf12b3b9e44f218f39115
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-11-11 13:54:54 -0500

    sumAll: Pass test 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,29 +1,37 @@
 const sumAll = function(num1, num2) { 
 
-  let greaterNumber;
-  let lesserNumber;
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
 
-  function compareNumbers() {
-    if (num1 > num2) {
-      greaterNumber = num1;
-      lesserNumber = num2;
-    } else {
-      greaterNumber = num2;
-      lesserNumber = num1;
+  } else {
+
+    let greaterNumber;
+    let lesserNumber;
+
+    function compareNumbers() {
+      if (num1 > num2) {
+        greaterNumber = num1;
+        lesserNumber = num2;
+      } else {
+        greaterNumber = num2;
+        lesserNumber = num1;
+      }
     }
+
+    compareNumbers();
+
+    let finalSum = 0;
+    let i = lesserNumber;
+
+    while (i <= greaterNumber) {
+      finalSum += i;
+
+      i++;
+    }
+
+    return finalSum;
   }
-
-  compareNumbers();
-
-  let finalSum = 0;
-  let i = lesserNumber;
-
-  while (i <= greaterNumber) {
-    finalSum += i;
-
-    i++;
-  }
-  return finalSum;
+  
 };
 
 // Do not edit below this line
