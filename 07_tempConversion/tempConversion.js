55a040ce9d48a6b426ebbef1557b60cf10a8ad68
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-11-12 08:36:28 -0500

    tempConversion: Pass test 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFahrenheit) {
+
+  // °C = (°F - 32) × 5/9
+
+  console.log(parseInt((degreeFahrenheit - 32) * (5 / 9).toFixed(1)))
+  return parseInt((degreeFahrenheit - 32) * (5 / 9).toFixed(1)); 
 };
+
 
 const convertToFahrenheit = function() {
 };
