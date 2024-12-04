e17e36ae3ecd0a39208c28b2baef0353bd25aa8f
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-12-04 13:47:50 -0500

    js-exercises-calculator: pass factorial tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -20,8 +20,18 @@ const power = function(num1, num2) {
 	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1; // 0! = 1
+  }
+
+  const x = num - 1;
+
+  for (let i = x; i > 0; i--) {
+    num *= i; // first: num(4) * x(3) = 12. second: num(12) * x(2) = 24. third: num(24) * x(1) = 24. 4! = 24. 
+  }
+
+  return num;
 };
 
 // Do not edit below this line
