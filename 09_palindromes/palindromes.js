b88780d180252271f5d1006274847c92ff2fdf1e
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-12-04 17:34:48 -0500

    js-exercises-palindromes: pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/\s/g, '');
 
+  let onlyAlphaNumeric = string.split('').filter((character) => character.match(/^[a-z0-9]+$/g));
+
+  string = onlyAlphaNumeric.join('');
+
+  let stringReversed = onlyAlphaNumeric.reverse().join('');
+
+  if (string === stringReversed) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
