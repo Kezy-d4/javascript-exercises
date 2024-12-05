d8ea7c1d09eb2030ad69bbb776c9eaa218ef9ae9
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-12-05 13:52:02 -0500

    js-exercises-fibonacci: pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  // 1, 1, 2, 3, 5, 8,
+  let sequence = [0, 1, 1,];
 
+  if (index < 0) {
+    return 'OOPS';
+  }
+
+  if (index >= 0 && index <= 2) {
+    return sequence[index];
+  }
+
+  let lastIndex;
+  let secondToLastIndex;
+  let nextNum;
+
+  while (true) {
+    lastIndex = sequence.length - 1;
+    secondToLastIndex = sequence.length - 2;
+
+    nextNum = sequence[lastIndex] + sequence[secondToLastIndex];
+
+    sequence.push(nextNum);
+
+    if (sequence[index]) {
+      return sequence[index];
+    }
+  }
 };
 
 // Do not edit below this line
