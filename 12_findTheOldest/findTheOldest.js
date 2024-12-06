2c2b64203829bb8927e41b4b82c6c643a278b666
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-12-06 12:46:49 -0500

    js-exercises-findTheOldest: pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
 
+  function getAge(obj) {
+    if (!obj.yearOfDeath) {
+      return currentYear - obj.yearOfBirth;
+    } else {
+      return obj.yearOfDeath - obj.yearOfBirth;
+    }
+  }
+
+  let ages = people.map((person) => getAge(person));
+
+  let oldest = Math.max(...ages);
+
+  return people.find((person) => getAge(person) === oldest);
 };
 
 // Do not edit below this line
