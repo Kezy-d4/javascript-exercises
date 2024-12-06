49b84caed43bcd5d9dd8b88760e21273749010df
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-12-06 11:27:27 -0500

    js-exercises-fibonacci: adjust solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,31 +1,45 @@
-const fibonacci = function(index) {
-  // 1, 1, 2, 3, 5, 8,
-  let sequence = [0, 1, 1,];
+const fibonacci = function(fibonacciNumber) {
+  // 1, 1, 2, 3, 5, 8...
+  // let sequence = [0, 1, 1,];
 
-  if (index < 0) {
-    return 'OOPS';
+  // if (index < 0) {
+  //   return 'OOPS';
+  // }
+
+  // if (index >= 0 && index <= 2) {
+  //   return sequence[index];
+  // }
+
+  // let lastIndex;
+  // let secondToLastIndex;
+  // let nextNum;
+
+  // while (true) {
+  //   lastIndex = sequence.length - 1;
+  //   secondToLastIndex = sequence.length - 2;
+
+  //   nextNum = sequence[lastIndex] + sequence[secondToLastIndex];
+
+  //   sequence.push(nextNum);
+
+  //   if (sequence[index]) {
+  //     return sequence[index];
+  //   }
+  // }
+  if (typeof fibonacciNumber !== 'number') {
+    fibonacciNumber = parseInt(fibonacciNumber);
   }
 
-  if (index >= 0 && index <= 2) {
-    return sequence[index];
+  if (fibonacciNumber < 0) return 'OOPS';
+
+  const sequence = [0, 1];
+
+  for (let index = 2; index <= fibonacciNumber; index++) {
+    sequence[index] = sequence[index - 1] + sequence[index - 2];
   }
 
-  let lastIndex;
-  let secondToLastIndex;
-  let nextNum;
+  return sequence[fibonacciNumber];
 
-  while (true) {
-    lastIndex = sequence.length - 1;
-    secondToLastIndex = sequence.length - 2;
-
-    nextNum = sequence[lastIndex] + sequence[secondToLastIndex];
-
-    sequence.push(nextNum);
-
-    if (sequence[index]) {
-      return sequence[index];
-    }
-  }
 };
 
 // Do not edit below this line
